f2cf12dd8cdc8d9016b645bf463aa6d2357e1e4b
구혜지 <ioabcoi@etoos.com>
2023-03-16 13:48:33 +0900

    2023-03-16

--- common/js/index.js
+++ common/js/index.js
@@ -1,12 +1,10 @@
-// table link
+// index link
 $(function(){
-    $("table").each(function(){
-        $(this).find("> tbody tr").each(function(){
-            $(this).find("> td a").each(function(){
-                var $link = $(this);
-                var link = $link.text();
-                $link.attr("href", link);
-            });
+    $(".index_area ul li > div").each(function(){
+        $(this).find("> a").each(function(){
+            var $link = $(this);
+            var link = $link.text();
+            $link.attr("href", link);
         });
     });
 });
