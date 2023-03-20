631a3194fe41d25e6b8ce01ea34909904c2f002e
구혜지 <ioabcoi@etoos.com>
2023-03-20 17:44:12 +0900

    2023-03-20

--- common/js/index.js
+++ common/js/index.js
@@ -1,5 +1,5 @@
-// index link
 $(function(){
+    // index_link
     $(".index_area ul li > div").each(function(){
         $(this).find("> .link").each(function(){
             var $link = $(this);
@@ -10,39 +10,17 @@ $(function(){
         });
     });
 
-    // iframe
+    // mobile_preview
     $(".preview").on("click", function() {
         var href = $(this).attr("href");
         $("#mobile_preview").attr("src", href);
         return false;
     });
 
-    /*
-    // ajax
-    $(".preview").on("click", function() {
-        var href = $(this).attr("href");
-        $.ajax({
-            url : "href",
-            success : function(result) {
-                $(".mobile_box").html(result);
-            }
-        });
-        return false;
-    });  
-
-    // $("#mobile_preview").mCustomScrollbar();
-    $(".mobile_box").mCustomScrollbar();
-
-    $(".preview").on("click", function() {
-        var href = $(this).attr("href");
-        $(".mobile_box").load(href);
-        return false;
-    });
-    */
-
+    // mobile_size
     $(".mobile_size ul li a").on("click", function() {
-        // var w = Number($(this).find('.width').text()) + 20;
-        var w = Number($(this).find('.width').text());
+        var w = Number($(this).find('.width').text()) + 17;
+        // var w = Number($(this).find('.width').text());
         var h = Number($(this).find('.height').text());
         $(".mobile_box").width(w);
         $(".mobile_box").height(h);
