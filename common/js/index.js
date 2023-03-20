cb415cdf8e9de03fbd4b2d51ea56072eecfa7b3a
구혜지 <ioabcoi@etoos.com>
2023-03-20 09:00:44 +0900

    2023-03-17

--- common/js/index.js
+++ common/js/index.js
@@ -1,10 +1,51 @@
 // index link
 $(function(){
     $(".index_area ul li > div").each(function(){
-        $(this).find("> a").each(function(){
+        $(this).find("> .link").each(function(){
             var $link = $(this);
             var link = $link.text();
+            var $preview = $(this).prev('.preview');
             $link.attr("href", link);
+            $preview.attr("href", link);
         });
+    });
+
+    // iframe
+    $(".preview").on("click", function() {
+        var href = $(this).attr("href");
+        $("#mobile_preview").attr("src", href);
+        return false;
+    });
+
+    /*
+    // ajax
+    $(".preview").on("click", function() {
+        var href = $(this).attr("href");
+        $.ajax({
+            url : "href",
+            success : function(result) {
+                $(".mobile_box").html(result);
+            }
+        });
+        return false;
+    });  
+
+    // $("#mobile_preview").mCustomScrollbar();
+    $(".mobile_box").mCustomScrollbar();
+
+    $(".preview").on("click", function() {
+        var href = $(this).attr("href");
+        $(".mobile_box").load(href);
+        return false;
+    });
+    */
+
+    $(".mobile_size ul li a").on("click", function() {
+        // var w = Number($(this).find('.width').text()) + 20;
+        var w = Number($(this).find('.width').text());
+        var h = Number($(this).find('.height').text());
+        $(".mobile_box").width(w);
+        $(".mobile_box").height(h);
+        return false;
     });
 });
