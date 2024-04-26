e3c501307054693768e8e0567d364225948402d5
구혜지 <ioabcoi@etoos.com>
2024-04-26 16:42:10 +0900

    2024-04-26

--- html/include/foot.js
+++ html/include/foot.js
@@ -1,6 +1,6 @@
 (function () {
     var html = ''
-        + '    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>'
+        + '    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>'
         + '    <script type="text/javascript" src="../../common/js/code.js"></script>'
         ;
     document.write(html);
