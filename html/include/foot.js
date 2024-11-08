26ce64d62cf146ab05d5fc0ec546af49795e72bd
구혜지 <ioabcoi@etoos.com>
2024-11-08 11:38:03 +0900

    <script> <style> 수정

--- html/include/foot.js
+++ html/include/foot.js
@@ -1,7 +1,7 @@
 (function () {
     var html = ''
-        + '    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>'
-        + '    <script type="text/javascript" src="../../common/js/code.js"></script>'
+        + '    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>'
+        + '    <script src="../../common/js/code.js"></script>'
         ;
     document.write(html);
 })();
